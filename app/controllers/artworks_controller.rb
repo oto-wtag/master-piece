248512553ffec8e24e7dde353550f670fcb885fc
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-01-13 11:34:11 +0600

    feature: artwork update and delete

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,15 +1,12 @@
 class ArtworksController < ApplicationController
+  before_action :set_artwork, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @artworks = Artwork.includes(image_attachment: :blob).all.order(created_at: :desc)
     @artwork = Artwork.new
   end
 
   def show
-    @artwork = Artwork.find(params[:id])
-  end
-
-  def new
-    @artwork = Artwork.new
   end
 
   def create
@@ -26,12 +23,26 @@ class ArtworksController < ApplicationController
   end
 
   def update
+    if @artwork.update(artwork_params)
+      redirect_to artworks_path, notice: "Artwork was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    if @artwork.destroy
+      redirect_to artworks_path, notice: "Artwork was successfully deleted."
+    else
+      redirect_to artworks_path, alert: "Failed to delete artwork."
+    end
   end
 
   private
+
+  def set_artwork
+    @artwork = Artwork.find(params[:id])
+  end
 
   def artwork_params
     params.require(:artwork).permit(:title, :description, :image)
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+
+document.addEventListener("turbo:submit-start", (event) => {
+  const form = event.target;
+
+  if (form.hasAttribute("data-confirm")) {
+    const message = form.getAttribute("data-confirm");
+
+    if (!window.confirm(message)) {
+      event.preventDefault();
+    }
+  }
+});
