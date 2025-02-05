e3c1f7590ba79a7895bc1072599de8c833783ae5
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-02-05 17:33:14 +0600

    bug fix: added some frontend fixes along with translations

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,10 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
   validates :profile_photo, content_type: [ "image/png", "image/jpg", "image/jpeg" ], size: { less_than: 5.megabytes, message: "is too large" }
 
-  validates :phone_number, uniqueness: true, format: {
+  validates :phone_number, uniqueness: true, allow_blank: true, format: {
     with: /\A\+?\d{10,15}\z/,
     message: "must be a valid phone number (10-15 digits, optional + at start)"
-  }
+  }, if: -> { phone_number.present? }
 
   def send_confirmation_instructions
     UserMailerJob.perform_later(self)
